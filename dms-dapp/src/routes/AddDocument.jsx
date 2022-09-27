5ba27d48f2f3128f7a4e9097d14900b0cace66e7
Joshua C. Lumanog <lumanog.joshua05@gmail.com>
2022-09-28 05:47:47 +0800

    Created a new branch: Just finished making the first UI for file upload

--- dms-dapp/src/routes/AddDocument.jsx
+++ dms-dapp/src/routes/AddDocument.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import './routes-css/add-document.css';
+
 
 function AddDocument({userInstance}) {
   return (
-    <div>
-      <h1>Add Document:</h1>
+    <div className="top-container">
+      <div className="upload-container">
+        <div className="title-container">
+          <h3>Upload Document</h3>
+        </div>
+        <form className="flex-upload-container" >
+          <div className="flex-item1">
+            <label>Upload File(s): </label>
+            <input type="file" className="upload-btn-class"></input>
+          </div>
+          <div className="flex-item2">
+            <label>Choose Where to Pin Data:</label>
+            <div className="checkbox-container">
+              <input type="checkbox" name="web3storage" value="web3storage"/>
+              <label for="web3storage"> Web3Storage</label><br/>
+              <input type="checkbox" name="vehicle2" value="Car"/>
+              <label for="vehicle2"> My own IPFS node</label><br/>
+            </div>
+          </div>
+          <div className="flex-item-last">
+            <button type="submit" className="submit-btn">Upload</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
